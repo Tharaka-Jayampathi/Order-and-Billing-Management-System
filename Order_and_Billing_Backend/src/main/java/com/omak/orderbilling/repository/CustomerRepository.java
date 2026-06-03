7aae68411cc8a2746c23758d8e68e0d3375669a9
Gimhani-00 <wdcgjayarathna@gmail.com>
2026-06-03 21:18:25 +0530

    Delete Order_and_Billing_Backend directory

--- Order_and_Billing_Backend/src/main/java/com/omak/orderbilling/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.omak.orderbilling.repository;
-
-import com.omak.orderbilling.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
